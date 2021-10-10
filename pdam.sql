ab0df9d9b77bc16f82785c81ac907311e4522278
muhamadsafrudin <safrudinlaptop@gmail.com>
2021-10-10 11:44:19 +0700

    databese

--- pdam.sql
+++ pdam.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 01, 2021 at 07:53 AM
+-- Generation Time: Oct 10, 2021 at 06:42 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.0
 
@@ -57,7 +57,8 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2014_10_12_000000_create_users_table', 1),
 (2, '2014_10_12_100000_create_password_resets_table', 1),
 (3, '2019_08_19_000000_create_failed_jobs_table', 1),
-(6, '2021_06_21_103150_create_tb_penagihan_table', 2);
+(6, '2021_06_21_103150_create_tb_penagihan_table', 2),
+(7, '2021_10_08_080155_create_pesan_table', 3);
 
 -- --------------------------------------------------------
 
@@ -70,6 +71,31 @@ CREATE TABLE `password_resets` (
   `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pesan`
+--
+
+CREATE TABLE `pesan` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `from` int(11) DEFAULT NULL,
+  `to` int(11) DEFAULT NULL,
+  `pesan` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `waktu` datetime NOT NULL,
+  `status` int(11) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `pesan`
+--
+
+INSERT INTO `pesan` (`id`, `from`, `to`, `pesan`, `waktu`, `status`, `created_at`, `updated_at`) VALUES
+(156, 1, 4, 'tes', '2021-10-10 04:41:42', 0, NULL, '2021-10-09 21:42:35'),
+(157, 4, 1, 'oke masuk', '2021-10-10 04:42:19', 1, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -94,7 +120,6 @@ CREATE TABLE `tb_penagihan` (
 --
 
 INSERT INTO `tb_penagihan` (`id_pelanggan`, `nama_pelanggan`, `alamat_pelanggan`, `tanggal_awal`, `tanggal_akhir`, `jumlah_tunggakan`, `keterangan`, `created_at`, `updated_at`) VALUES
-('2252525', 'Feri', 'Pati', '2021-03-17', '2021-06-22', 90000, NULL, NULL, '2021-06-22 23:20:45'),
 ('225253434', 'Abel', 'Pati', '2021-05-13', '2023-05-22', 45000, NULL, NULL, '2021-06-22 00:04:55'),
 ('33333', 'fera', 'Kudus', '2021-10-22', '2021-06-22', 50000, NULL, NULL, NULL),
 ('4343435', 'heri', 'Kudus', '2021-06-21', '2021-06-26', 76000, NULL, NULL, '2021-06-21 22:58:02'),
@@ -124,8 +149,9 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `email`, `role`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
 (1, 'Administrator', 'admin@gmail.com', 1, NULL, '$2y$10$IF.poENBaUh5qUmrB8P3POLJXbOUGzAuiNs2W3HBZjOgM0JaeNiAq', NULL, '2021-06-21 06:58:38', '2021-06-21 21:45:07'),
-(3, 'safrudin1911', 'safrudin@gmail.com', 1, NULL, '$2y$10$4wDGeSyarwDIcv2PgqhnoeFwXbDwwYNkukNfnamv5VdrdB5RIWyvO', NULL, NULL, NULL),
-(4, 'wahyu', 'users@gmail.com', 2, NULL, '$2y$10$22JAyqirryKpg9sbPp7AH.2VTdVRsCu.aQbwLaNbe5zdQudIXfi8a', NULL, NULL, NULL);
+(4, 'wahyu', 'users@gmail.com', 2, NULL, '$2y$10$rjor3GcsfdEjRMwsHbnPYuQ.vFwP0qyRvHFg7A8cQt6TwIgeYa8wK', NULL, NULL, '2021-10-08 02:19:44'),
+(5, 'wagimen', 'wagimen@email.com', 3, NULL, '$2y$10$eRN757dm.tbIVqsn8ZQWX.3FNQ9sb2lLdBv9wN8o/MltwS70kpVSy', NULL, NULL, '2021-10-08 23:35:57'),
+(6, 'Sopian', 'users1@gmail.com', 2, NULL, '$2y$10$JMdoWFz/gt3Wcoezx96f7exIauRY8H0Qdu1IYW41imTWLQvgmMmmm', NULL, NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -149,6 +175,12 @@ ALTER TABLE `migrations`
 --
 ALTER TABLE `password_resets`
   ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indexes for table `pesan`
+--
+ALTER TABLE `pesan`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `tb_penagihan`
@@ -177,13 +209,19 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `pesan`
+--
+ALTER TABLE `pesan`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=158;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
